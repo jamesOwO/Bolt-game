b236bb0504fbe341f5a94346061f1a8e99414dfd
jamesOwO <113694418+jamesOwO@users.noreply.github.com>
2023-01-25 13:03:05 +0000

    no message

--- template for visual script game/Assets/Scenes/RandomMathsEquation.cs
+++ template for visual script game/Assets/Scenes/RandomMathsEquation.cs
@@ -7,20 +7,20 @@ public class RandomMathsEquation : MonoBehaviour
 {
     public Text question;
     int number1, number2, total;
-    // Start is called before the first frame update
+    public InputField input;
+
     void Start()
     {
         number1 = Random.Range(0, 13);
         number2 = Random.Range(0, 13);
         total = number1 * number2;
         question.text = number1 + " X " + number2;
-        Debug.Log(question.text);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(input.text);
     }
-     
+
 }
